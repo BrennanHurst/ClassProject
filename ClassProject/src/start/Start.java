dd6580c2f48be9e470c402851ae9ec35ffe9ec20
BrennanHurst <hurstbh11@gmail.com>
2017-01-25 11:44:45 -0500

    Initial commit

---
+++ ClassProject/src/start/Start.java
@@ -0,0 +1,13 @@
+package start;
+
+import jframe.JFrameStart;
+
+public class Start {
+
+	
+	public static void main(String args[])
+	{
+		JFrameStart jf = new JFrameStart();
+		System.out.println("This is the second commit");
+	}
+}
